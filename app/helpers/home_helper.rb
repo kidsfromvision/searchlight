3845618acce14d999d6eaa9913eb3e90c4545ea0
Sam Brownstone <brownstone@hey.com>
2023-01-24 18:56:49 +0000

    fix song status not being readable

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def get_readable_status(status)
+    readable_statuses = {
+      "not_contacted" => "Not contacted",
+      "contacted" => "Contacted",
+      "replied" => "Replied",
+      "offered" => "Offered",
+      "accepted" => "Accepted",
+    }
+    return readable_statuses[status]
+  end
 end
