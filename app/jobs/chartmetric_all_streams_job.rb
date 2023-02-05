4f9e6d75d29cea38326a5b9957d9e0563f85efcc
Sam Brownstone <brownstone@hey.com>
2023-02-05 18:56:04 +0000

    stop DRY violation

--- app/jobs/chartmetric_all_streams_job.rb
+++ app/jobs/chartmetric_all_streams_job.rb
@@ -3,17 +3,6 @@ class ChartmetricAllStreamsJob < ApplicationJob
 
   def perform
     songs = Song.all
-    songs.each do |song|
-      song.broadcast_streams_loading
-      streams = ChartmetricRequestManager.get_streams(song)
-      if streams.length > 0
-        song.song_streams.create(
-          streams.map do |stream|
-            { streams: stream["value"], date: stream["timestp"] }
-          end,
-        )
-      end
-      song.broadcast_streams
-    end
+    songs.each { |song| ChartmetricSingleStreamsJob.perform_now(song) }
   end
 end
