4d7e75b91d5127c1e9f3cd372438c7e6ec13de9a
Sam Brownstone <brownstone@hey.com>
2023-02-05 01:48:35 +0000

    add stream calls not firing if they wouldn't get new data

--- app/jobs/chartmetric_stream_job.rb
+++ app/jobs/chartmetric_stream_job.rb
@@ -1,39 +1,16 @@
 class ChartmetricStreamJob < ActiveJob::Base
   queue_as :default
 
-  def perform(song, user)
-    stream_event = song.stream_events.create(provider: "chartmetric")
+  def perform(song)
     song.broadcast_streams_loading
-    song_streams =
-      song.song_streams.where(provider: "chartmetric").order("date DESC")
-
-    if song_streams && song_streams.length > 0
-      since_query =
-        "&since=#{(song_streams[0].date + 1.days).strftime("%Y-%m-%d")}"
-    else
-      since_query = ""
-    end
-
-    headers = { "Authorization" => "Bearer #{ChartmetricAuthManager.token}" }
-    response =
-      HTTParty.get(
-        "https://api.chartmetric.com/api/track/#{song.spotify_id}/spotify/stats/most-history?isDomainId=true&type=streams#{since_query}",
-        headers: headers,
-      )
-
-    if response && response["obj"].length > 0
-      data = response["obj"].first["data"]
+    streams = ChartmetricRequestManager.get_streams(song)
+    if streams.length > 0
       song.song_streams.create(
-        data.map do |stream|
+        streams.map do |stream|
           { streams: stream["value"], date: stream["timestp"] }
         end,
       )
-      stream_event.status = "success"
-    else
-      stream_event.status = "failed"
     end
-
     song.broadcast_streams
-    stream_event.save
   end
 end
---
+++ app/services/chartmetric_request_manager.rb
@@ -0,0 +1,35 @@
+class ChartmetricRequestManager
+  class << self
+    def get_streams(song)
+      last_updated = last_update_date(song)
+      puts "LAST UPDATED", last_updated
+      if last_updated.nil? || Time.now > last_updated + 1.days
+        stream_event = song.stream_events.create(provider: "chartmetric")
+        since_query =
+          last_updated.nil? ? "" : "&since=#{last_updated.strftime("%Y-%m-%d")}"
+        headers = {
+          "Authorization" => "Bearer #{ChartmetricAuthManager.token}",
+        }
+        response =
+          HTTParty.get(
+            "https://api.chartmetric.com/api/track/#{song.spotify_id}/spotify/stats/most-history?isDomainId=true&type=streams#{since_query}",
+            headers: headers,
+          )
+        stream_event.status = "success"
+        stream_event.save
+        return response["obj"].first["data"] if response["obj"].length > 0
+
+        stream_event.status = "failed"
+        stream_event.save
+      end
+      []
+    end
+
+    private
+
+    def last_update_date(song)
+      streams = song.song_streams
+      streams.last.nil? ? nil : streams.last.date
+    end
+  end
+end
