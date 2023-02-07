dae2f8e781de7af4ee364a99e3c1469882535c82
Sam Brownstone <brownstone@hey.com>
2023-02-07 14:40:59 +0000

    add user role to user table

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,2 @@
 module SongsHelper
-  def get_readable_status(status)
-    readable_statuses = {
-      "watching" => "Watching",
-      "messaged" => "Messaged",
-      "in_conversation" => "In conversation",
-      "offered" => "Offered",
-      "closed" => "Closed",
-    }
-    return readable_statuses[status]
-  end
 end
--- app/helpers/tracked_song_helper.rb
+++ app/helpers/tracked_song_helper.rb
@@ -1,2 +1,12 @@
 module TrackedSongHelper
+  def get_readable_status(status)
+    readable_statuses = {
+      "watching" => "Watching",
+      "messaged" => "Messaged",
+      "in_conversation" => "In conversation",
+      "offered" => "Offered",
+      "closed" => "Closed",
+    }
+    return readable_statuses[status]
+  end
 end
