5c4b4fa093b00682f9c664f85abf87fe535a0fbd
Sam Brownstone <brownstone@hey.com>
2023-02-07 18:04:50 +0000

    add asc ordering on each column of leaderboard

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,30 @@ class SongsController < ApplicationController
       @songs = current_user.songs
     end
   end
+
+  def list
+    if current_user.label_id
+      label = current_user.label
+      songs = label.songs
+    else
+      label = nil
+      songs = current_user.songs
+    end
+    if params[:column] == "streams"
+      songs =
+        songs.sort_by do |song|
+          song.recent_streams.last.streams - song.recent_streams.first.streams
+        end
+    elsif params[:column] == "added_by" # only happens if the user is part of a label
+      songs =
+        songs
+          .joins(tracked_songs: :user)
+          .where(tracked_songs: { label_id: label.id })
+          .order("users.name asc")
+    else
+      songs = songs.order("#{params[:column]} asc")
+    end
+
+    render(partial: "songs/songs", locals: { songs: songs })
+  end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,5 @@
 module SongsHelper
+  def sort_link(column:, label:)
+    link_to(label, list_songs_path(column: column))
+  end
 end
