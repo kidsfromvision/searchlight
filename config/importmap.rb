84429504815f10ea6a6574adc11a1faa3eedaaea
Sam Brownstone <brownstone@hey.com>
2023-02-20 13:30:27 +0000

    use ajax request to update status

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,6 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "debounce", to: "https://ga.jspm.io/npm:debounce@1.2.1/index.js"
 pin "mrujs", to: "https://ga.jspm.io/npm:mrujs@0.7.1/dist/mrujs.module.js"
 pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.6.1/dist/morphdom.js"
+pin "@rails/ujs",
+    to:
+      "https://ga.jspm.io/npm:@rails/ujs@7.0.4-2/lib/assets/compiled/rails-ujs.js"
