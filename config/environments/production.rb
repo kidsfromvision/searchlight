ef1bd2a9f2172880f36f550f1ed4d4c13290d147
Sam Brownstone <brownstone@hey.com>
2023-03-02 15:48:29 +0000

    fix base url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,6 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: 587,
     user_name: "apikey",
-    domain: "kidsfromvision.com",
     password: ENV["SENDGRID_API_KEY"],
     enable_starttls_auto: true,
     authentication: "plain",
