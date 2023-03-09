5e9edb433cae4dbe0cd12850b4bbc9afe5193483
Sam Brownstone <brownstone@hey.com>
2023-03-09 00:57:37 +0000

    fix docker-entrypoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create a main sample label.
+Label.create!(name: "Kids from Vision")
+
+# Create a main sample user.
+User.create!(name: "Sam Brownstone", email: "brownstone@hey.com", password: "123456", label_id: 1, role: "label_admin")
