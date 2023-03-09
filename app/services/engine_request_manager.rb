ee4fb1fce63aa5a38b7cbd4934b051191fdcf89f
Sam Brownstone <brownstone@hey.com>
2023-03-09 01:10:20 +0000

    change ip to gluetun container

--- app/services/engine_request_manager.rb
+++ app/services/engine_request_manager.rb
@@ -1,7 +1,7 @@
 class EngineRequestManager
   class << self
     def create_artist_metadata(artist_id)
-      pathfinder_url = "http://127.0.0.1:8081/artist/create_metadata"
+      pathfinder_url = "http://172.18.0.6:8080/artist/create_metadata"
       url = "#{pathfinder_url}/?artist_id=#{artist_id}"
       response = HTTParty.post(url)
       response.parsed_response
