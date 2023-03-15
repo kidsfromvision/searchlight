309ec7ace834a74d8529f07fcbbbe9031db95d1b
Sam Brownstone <brownstone@hey.com>
2023-03-15 00:09:19 +0000

    Fix url

--- app/services/engine_request_manager.rb
+++ app/services/engine_request_manager.rb
@@ -8,7 +8,7 @@ class EngineRequestManager
     end
 
     def get_song_playcount(spotify_id)
-      url = "http://127.0.0.1:3010/spotify/song/#{spotify_id}/playcount"
+      url = "http://172.18.0.6:8080/spotify/song/#{spotify_id}/playcount"
       response = HTTParty.get(url)
       puts "RESPONSE, #{response.parsed_response["playcount"]}"
       response.parsed_response["playcount"]
